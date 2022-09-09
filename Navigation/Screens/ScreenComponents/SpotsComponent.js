b926f7b261be7a11eca22224b8c9ca28fe28498b
kiel <kielmitchell8@gmail.com>
2022-09-09 18:01:39 +0100

    further styling fixes to the UserScreen SpotsComponent

--- Navigation/Screens/ScreenComponents/SpotsComponent.js
+++ Navigation/Screens/ScreenComponents/SpotsComponent.js
@@ -8,16 +8,18 @@ import {
 } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
-const SpotsItem = ({ spot }) => (
+const SpotsItem = ({ spot, handleFavPress }) => (
   <View
     className="bg-slate-400 border-slate-400 flex-col items-center justify-evenly border-2  pl-2 mt-3 ml-5 mr-5 rounded-md"
     key={spot.item.id}
   >
     <View className="flex-row">
-      <View className=" bg-slate-400 border-slate-400 flex-col flex-wrap items-start justify-evenly">
-        <Text className="text-m text-white font-medium mt-1">
-          Name: {spot.item.name}
-          {",  "}
+      <View className=" bg-slate-400 border-slate-400 flex-col  items-start justify-evenly">
+        <Text className="text-m text-white flex-wrap font-medium mt-1">
+          Name:{" "}
+          {spot.item.name.length > 20
+            ? spot.item.name.substring(0, 20) + "... "
+            : spot.item.name + ",  "}
         </Text>
         <Text className="text-m text-white font-medium mt-1">
           latitude: {spot.item.latitude}
@@ -39,7 +41,13 @@ const SpotsItem = ({ spot }) => (
         </View>
       </View>
       <SafeAreaView className="flex-col justify-center items-center">
-        <TouchableOpacity className=" ml-2 rounded-md bg-slate-600 h-14 w-14 justify-center items-center">
+        <TouchableOpacity
+          className=" ml-2 rounded-md bg-slate-600 h-14 w-14 justify-center items-center"
+          onPress={(event) => {
+            event.spot_id = spot.item.spot_id;
+            handleFavPress(event);
+          }}
+        >
           <Ionicons name="star" size={20} color="yellow" />
           <Text className="text-m text-white font-medium text-center">Add</Text>
         </TouchableOpacity>
